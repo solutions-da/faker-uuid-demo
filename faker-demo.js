34d409b58b54ec72ca0d43d45d7e23077f116366
fagoner <farendaway@gmail.com>
2021-12-09 10:17:16 -0600

    first commit

---
+++ faker-demo.js
@@ -0,0 +1,13 @@
+const faker = require('faker');
+
+const  randomName = faker.name.findName(); // Rowan Nikolaus
+
+const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+const randomCard = faker.helpers.createCard(); 
+
+
+
+console.log(randomName);
+console.log(randomEmail);
+console.log(randomCard);
+
---
+++ index.js
@@ -0,0 +1,6 @@
+const { v4: uuidv4 } = require('uuid');
+const nombreFalso = `nombre_${uuidv4()}`; // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+
+
+console.log(nombreFalso);
+
